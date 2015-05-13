503dcf3a2a07f418ae14f317c6dcf65df9e74861
anes89cok <anes89cok@gmail.com>
2015-05-13 14:34:25 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+	$adresa="3918 Irwin Simpson Rd. Mason, Ohio"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Ohio";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=+15135457134;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+    $transitionefect1 = 'fade';
+?>
